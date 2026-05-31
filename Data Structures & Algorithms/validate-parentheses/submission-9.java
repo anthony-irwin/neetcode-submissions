1112ec1af67105829badb5977354e6b787ceae56
Anthony Irwin <anthonyofirwin@gmail.com>
2026-05-31 15:09:38 -0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+
+        if (s.charAt(0) == ')' || s.charAt(0) == '}' || s.charAt(0) == ']') {
+            return false;
+        }
+
+        if (s.charAt(s.length() - 1) == '(' || s.charAt(s.length() - 1) == '{' || s.charAt(s.length() - 1) == '[') {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == ')') {
+                if (stack.pop() == '(') {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            if (c == '}') {
+                if (stack.pop() == '{') {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            if (c == ']') {
+                if (stack.pop() == '[') {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            stack.push(c);
+        }
+        return stack.size() == 0;
+    }
+}
